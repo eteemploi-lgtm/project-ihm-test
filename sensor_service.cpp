019b3239c4d7b737dda83baa35fdd748ab9253af
eteemploi-lgtm <eteemploi@gmail.com>
2026-04-17 08:07:35 +0200

    Delete sensor_service.cpp

--- sensor_service.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../include/services/sensor_service.hpp"
-
-SensorService::SensorService(DeviceManager& device_manager) : device_manager_(device_manager) {}
-
-std::string SensorService::get_sensors_json() {
-    return device_manager_.get_sensors().to_json();
-}
