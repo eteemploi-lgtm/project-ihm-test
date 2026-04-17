6e13c00419dd6cafc9fddae5ddfd0e2db70d4bb3
eteemploi-lgtm <eteemploi@gmail.com>
2026-04-17 08:08:04 +0200

    Delete sensor_routes.cpp

--- sensor_routes.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../include/api/sensor_routes.hpp"
-
-SensorRoutes::SensorRoutes(SensorService& service) : service_(service) {}
-
-std::string SensorRoutes::handle_get_sensors() {
-    return service_.get_sensors_json();
-}
