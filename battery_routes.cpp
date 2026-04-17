704f9783a94813f7720dfb66b6a65d324d32b6d4
eteemploi-lgtm <eteemploi@gmail.com>
2026-04-17 07:53:36 +0200

    Delete battery_routes.cpp

--- battery_routes.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../include/api/battery_routes.hpp"
-
-BatteryRoutes::BatteryRoutes(BatteryService& service) : service_(service) {}
-
-std::string BatteryRoutes::handle_get_battery() {
-    return service_.get_battery_json();
-}
